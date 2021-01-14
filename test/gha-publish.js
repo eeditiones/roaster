36d3be3393023342221cccd2c2540d204b62aaf5
Juri Leino <github@line-o.de>
2021-01-14 22:36:46 +0100

    fix(release): custom publish task syntax

--- test/gha-publish.js
+++ test/gha-publish.js
@@ -1,9 +1,15 @@
 const execa = require('execa');
 
-async function publish (pluginConfig, {cwd, env}) {
-    const packageResult = execa('npm', ['run', 'build'], {cwd, env});
+async function publish ({pkgRoot}, {cwd, env, stdout, stderr, nextRelease: {version}, logger}) {
+    const basePath = pkgRoot ? path.resolve(cwd, pkgRoot) : cwd;
+
+    logger.log('Create XAR in version %s in %s', version, basePath);
+
+    const packageResult = execa('npm', ['run', 'build'], {cwd:basePath, env});
+
     packageResult.stdout.pipe(stdout, {end: false});
     packageResult.stderr.pipe(stderr, {end: false});
+  
     await packageResult;
 }
 
